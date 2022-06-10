29157a5f4408ce5f3a90711dd739996de9a7fdc3
0101prashant <106469868+0101prashant@users.noreply.github.com>
2022-06-10 14:05:08 +0530

    Update and.cpp

--- Bitwise-Operator/and.cpp
+++ Bitwise-Operator/and.cpp
@@ -2,8 +2,8 @@
 
 int main(){
 
-    int a  = 1; // 0001 bits repersentation
-    int b = 3; //  0011 bits repersentation
+    int a  = 1; // 0001 bits representation
+    int b = 3; //  0011 bits representation
 
     printf("%d",(a&b));
 
