87949f36cd64a02b70f924897ed5df516d76c2dd
0101prashant <106469868+0101prashant@users.noreply.github.com>
2022-06-10 14:05:26 +0530

    Update or.cpp

--- Bitwise-Operator/or.cpp
+++ Bitwise-Operator/or.cpp
@@ -2,8 +2,8 @@
 
 int main(){
 
-    int a  = 5; // 0101 bits repersentation
-    int b = 3; //  0011 bits repersentation
+    int a  = 5; // 0101 bits representation
+    int b = 3; //  0011 bits representation
 
     printf("%d",(a|b));
 
